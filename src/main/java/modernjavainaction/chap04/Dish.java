7572cd7fc260cbb9e9eedab68340f9d7b0c44923
jipark1106 <jipark1106@gmail.com>
2023-11-15 16:47:07 +0900

    ~chapter4

--- src/main/java/modernjavainaction/chap04/Dish.java
+++ src/main/java/modernjavainaction/chap04/Dish.java
@@ -1,37 +1,21 @@
 package modernjavainaction.chap04;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.Arrays;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
 public class Dish {
 
   private final String name;
   private final boolean vegetarian;
   private final int calories;
   private final Type type;
-
-  public Dish(String name, boolean vegetarian, int calories, Type type) {
-    this.name = name;
-    this.vegetarian = vegetarian;
-    this.calories = calories;
-    this.type = type;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public boolean isVegetarian() {
-    return vegetarian;
-  }
-
-  public int getCalories() {
-    return calories;
-  }
-
-  public Type getType() {
-    return type;
-  }
 
   public enum Type {
     MEAT,
--- src/main/java/modernjavainaction/chap04/StreamBasic.java
+++ src/main/java/modernjavainaction/chap04/StreamBasic.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class StreamBasic {
 
@@ -18,6 +19,10 @@ public class StreamBasic {
 
     // 자바 8
     getLowCaloricDishesNamesInJava8(Dish.menu).forEach(System.out::println);
+    System.out.println("---");
+    getLowCaloricDishesNamesInJava8Version2(Dish.menu).forEach(System.out::println);
+    System.out.println("-------");
+    getLowCaloricDishesNamesInJava8Version3(Dish.menu);
   }
 
   public static List<String> getLowCaloricDishesNamesInJava7(List<Dish> dishes) {
@@ -27,7 +32,7 @@ public class StreamBasic {
         lowCaloricDishes.add(d);
       }
     }
-    List<String> lowCaloricDishesName = new ArrayList<>();
+    List<String> lowCaloricDishesName = new ArrayList<>();    //가비지 변수
     Collections.sort(lowCaloricDishes, new Comparator<Dish>() {
       @Override
       public int compare(Dish d1, Dish d2) {
@@ -41,11 +46,37 @@ public class StreamBasic {
   }
 
   public static List<String> getLowCaloricDishesNamesInJava8(List<Dish> dishes) {
-    return dishes.stream()
-        .filter(d -> d.getCalories() < 400)
-        .sorted(comparing(Dish::getCalories))
+    return dishes.stream()                //스트림을 얻음
+        .filter(d -> d.getCalories() < 400) //중간연산
+        .sorted(comparing(Dish::getCalories))//중간연산
         .map(Dish::getName)
         .collect(toList());
+  }
+
+  public static List<String> getLowCaloricDishesNamesInJava8Version2(List<Dish> dishes){
+    return dishes.stream()
+            .filter(d -> d.getCalories() < 400)
+            .sorted(Comparator.comparing(d -> d.getCalories()))
+            .map(d -> d.getName())
+            .collect(Collectors.toList());
+  }
+
+  public static List<String> getLowCaloricDishesNamesInJava8Version3(List<Dish> dishes){
+
+    List<String> nameList =
+            dishes.stream()
+                    .filter(dish -> {
+                      System.out.println("filtering :: " + dish.getName());
+                      return dish.getCalories() > 300;
+                    })
+                    .map(dish -> {
+                      System.out.println("mapping :: " + dish.getName());
+                      return dish.getName();
+                    })
+                    .collect(Collectors.toList());
+
+
+    return nameList;
   }
 
 }
